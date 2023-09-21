552a3d5021533e8d53d48eeaffe4df8627f8dc6a
femke77 <megan.meyers.388@gmail.com>
2023-09-20 22:52:31 -0700

    fixing cannot get

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import {
   createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Signin from './pages/SignIn';
@@ -68,10 +68,7 @@ function App() {
                 path="/profile" 
                 element={<Profile />}
               />
-              {/* <Route 
-                path="/thoughts/:thoughtId" 
-                element={<SingleThought />}
-              /> */}
+    
             </Routes>
           </div>
         </div>
